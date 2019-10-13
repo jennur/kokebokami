e347172c0b78dc6266217f0feb84afaee1ef4460
Jenny Bonsak <jenny.bonsak@gmail.com>
2019-10-13 20:58:35 +0200

    Generate routes

--- frontend/nuxt.config.js
+++ frontend/nuxt.config.js
@@ -2,7 +2,17 @@ require("dotenv").config();
 
 export default {
   mode: "spa",
-  generate: {},
+  generate: {
+    routes: context => {
+      const { store } = context;
+      const recipes = store.getters.recipes;
+
+      const routes = recipes.map(recipe => {
+        return "/recipes/" + recipe.id;
+      });
+      return routes;
+    }
+  },
 
   /*
    ** Headers of the page
