2981b4bb98e88e0531c7687e7b6c860c948f896e
Jenny Bonsak <jenny.bonsak@gmail.com>
2019-10-19 23:43:30 +0200

    Testing for netlify #124897354332421332321rw4t22

--- frontend/nuxt.config.js
+++ frontend/nuxt.config.js
@@ -1,5 +1,5 @@
 require("dotenv").config();
-var { google } = require("googleapis");
+/*var { google } = require("googleapis");
 const axios = require("axios");
 
 // Initialize the service account key object.
@@ -50,20 +50,72 @@ let routesPromise = new Promise((resolve, reject) => {
         .catch(error => console.log("AXIOS ERROR::: " + error));
     }
   });
-});
+});*/
 
 export default {
   mode: "spa",
   generate: {
-    fallback: true
-    /*
+    fallback: true,
+
     routes: function(callback) {
+      var { google } = require("googleapis");
+      const axios = require("axios");
+
+      // Initialize the service account key object.
+      const serviceAccount = {
+        type: "service_account",
+        project_id: process.env.PROJECT_ID,
+        private_key_id: process.env.PRIVATE_KEY_ID,
+        private_key: process.env.PRIVATE_KEY,
+        client_email: process.env.CLIENT_EMAIL,
+        client_id: process.env.CLIENT_ID,
+        auth_uri: process.env.AUTH_URI,
+        token_uri: process.env.TOKEN_URI,
+        auth_provider_x509_cert_url: process.env.AUTH_PROVIDER_CERT_URL,
+        client_x509_cert_url: process.env.CLIENT_CERT_URL
+      };
+      // Define the required scopes.
+      var scopes = [
+        "https://www.googleapis.com/auth/userinfo.email",
+        "https://www.googleapis.com/auth/firebase.database"
+      ];
+
+      // Authenticate a JWT client with the service account.
+      var jwtClient = new google.auth.JWT(
+        serviceAccount.client_email,
+        null,
+        serviceAccount.private_key,
+        scopes
+      );
+
+      // Use the JWT client to generate an access token.
+      let routesPromise = new Promise((resolve, reject) => {
+        jwtClient.authorize((error, tokens) => {
+          let routes = [];
+
+          if (error) return error;
+          else {
+            axios
+              .get(
+                "https://kokebokami-e788f.firebaseio.com/recipes.json?access_token=" +
+                  tokens.access_token
+              )
+              .then(response => {
+                Object.keys(response.data).forEach(key => {
+                  routes.push("/recipes/" + key);
+                });
+                resolve(routes);
+              })
+              .catch(error => console.log("AXIOS ERROR::: " + error));
+          }
+        });
+      });
       routesPromise
         .then(routes => {
           callback(null, routes);
         })
         .catch(callback);
-    }*/
+    }
   },
 
   /*
