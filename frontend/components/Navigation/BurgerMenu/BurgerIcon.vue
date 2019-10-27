8c9230a359de6c72931e1ab255f9a38899d16b50
Jenny Bonsak <jenny.bonsak@gmail.com>
2019-10-27 21:06:21 +0100

    Build mobile menu

---
+++ frontend/components/Navigation/BurgerMenu/BurgerIcon.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="burger-icon">
+    <span :class="'burger-icon__slice ' + (open ? 'burger-icon__slice--open' : '') " />
+  </div>
+</template>
+<script>
+export default {
+  name: "burger-icon",
+  props: {
+    open: { type: Boolean, default: false }
+  }
+};
+</script>
---
+++ frontend/components/Navigation/Navigation.vue
@@ -0,0 +1,78 @@
+<template>
+  <nav class="navigation-menu margin--auto margin-bottom--xlarge">
+    <h1 class="navigation-menu__logo">
+      <nuxt-link class="navigation-menu__logo-link" to="/">Kokebokami</nuxt-link>
+    </h1>
+    <burger-icon @click.native="toggleMenu" :open="open" />
+    <div
+      :class="'navigation-menu__list-container ' + (open ? 'navigation-menu__list-container--open' : '')"
+    >
+      <ul class="navigation-menu__list">
+        <li v-for="menuItem in menuItems" :key="menuItem.name">
+          <img
+            v-if="menuItem.img"
+            class="google-profile-picture"
+            :src="menuItem.img"
+            alt="Google profile picture"
+          />
+          <nuxt-link
+            class="navigation-menu__link"
+            :to="menuItem.link"
+            @click.native="()=>{open = false}"
+          >{{menuItem.name}}</nuxt-link>
+        </li>
+        <li v-if="user">
+          <button class="logout-button" @click="logOut">Log out</button>
+        </li>
+      </ul>
+    </div>
+  </nav>
+</template>
+
+<script>
+import { user } from "~/mixins/getCurrentUser.js";
+import BurgerIcon from "./BurgerMenu/BurgerIcon.vue";
+
+export default {
+  name: "navigation",
+  components: {
+    BurgerIcon
+  },
+  data() {
+    return { open: false };
+  },
+  mixins: [user],
+  computed: {
+    menuItems() {
+      let menuItems = [];
+      if (this.user !== null && this.user !== undefined) {
+        menuItems = [
+          {
+            link: "/account",
+            name: "My account",
+            img: this.user.profileImg
+          },
+          { link: "/account/my-recipes", name: "My recipes" }
+        ];
+      } else {
+        menuItems = [
+          { link: "/", name: "Home" },
+          { link: "/login", name: "Log in" }
+        ];
+      }
+      return menuItems;
+    }
+  },
+  methods: {
+    toggleMenu() {
+      this.open = !this.open;
+      document.getElementById("layout").classList.toggle("translate-with-menu");
+    },
+    logOut() {
+      this.$store.dispatch("USER_SIGN_OUT");
+      this.$router.push("/");
+      this.open = false;
+    }
+  }
+};
+</script>
