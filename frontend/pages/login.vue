1d7b35a807b6f9c4b5add25c3b502240b2d4b71f
Jenny Bonsak <jenny.bonsak@gmail.com>
2019-11-01 21:28:38 +0100

    Style login/signup

--- frontend/pages/login.vue
+++ frontend/pages/login.vue
@@ -1,10 +1,7 @@
 <template>
   <div class="container container--center margin--auto">
-    <h2>Log in to see kokeboka di</h2>
-
-    <div class="flex-center-container flex-center-container--tablet-width margin--auto">
+    <div class="flex-center-container margin--auto">
       <login-container class="flex-order--tablet-two" />
-
       <kokebokapen class="graphic graphic--kokebokapen flex-order--tablet-one" />
     </div>
   </div>
