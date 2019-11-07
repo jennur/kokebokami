e625c832a1d8ba9a82ada8864daae38dd4fe16ca
Jenny Bonsak <jenny.bonsak@gmail.com>
2019-11-07 18:15:32 +0100

    Reorganize recipe paths2

---
+++ frontend/pages/recipes/index.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="container">
+    <nuxt-child />
+  </div>
+</template>
+
+<script>
+export default {
+  name: "recipes",
+  components: {},
+  computed: {}
+};
+</script>
+
