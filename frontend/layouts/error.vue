d061af145b4371ab9aec89e93aa3abe43aa4e9aa
Jenny Bonsak <jenny.bonsak@gmail.com>
2019-11-10 20:55:18 +0100

    Add error page

---
+++ frontend/layouts/error.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="tablet-width padding-horizontal--large margin--auto">
+    <div v-if="error.statusCode === 404">
+      <h1>404 Whoops!</h1>
+      <h3>It looks like you're trying to access a page that does not exist, or that you don't have access to...</h3>
+    </div>
+    <h2 v-else>An error occurred</h2>
+    <nuxt-link to="/">Take me back to the front page</nuxt-link>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["error"],
+  layout: "default"
+};
+</script>
