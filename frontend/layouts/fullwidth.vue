f7e630c4f935ddcaa1c8c585da02cab64664e45a
Jenny Bonsak <jenny.bonsak@gmail.com>
2020-01-27 21:40:36 +0100

    Fix layout bug

--- frontend/layouts/fullwidth.vue
+++ frontend/layouts/fullwidth.vue
@@ -1,21 +1,20 @@
 <template>
   <div>
     <navigation />
-    <div class="tablet-width margin--auto padding-horizontal--large">
+    <div class="full-width margin--auto">
       <nuxt />
     </div>
     <footer-component />
     <cookie-consent />
+    <script id="dsq-count-scr" src="//kokebokami.disqus.com/count.js" async></script>
   </div>
 </template>
 <script>
 import CookieConsent from "~/components/CookieConsent.vue";
 export default {
-  name: "default-layout",
+  name: "fullwidth",
   components: {
     CookieConsent
   }
 };
 </script>
-
-
