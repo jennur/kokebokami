3e57634322ecf5b92f1c9e4e46719f163450b6c6
Jenny Bonsak <jenny.bonsak@gmail.com>
2020-02-06 18:26:22 +0100

    Add index info section to component

---
+++ frontend/components/InitialInfoSection/InititalInfoSection.vue
@@ -0,0 +1,34 @@
+<template>
+  <section class="tablet-width margin--auto padding-horizontal--large margin-top--xxlarge">
+    <div
+      class="flex-center-container flex-center-container--column mobile-width padding--none margin--auto"
+    >
+      <h1
+        class="heading--display-font padding-horizontal--large"
+      >It's time to digitalize your cook book!</h1>
+      <p class="padding-horizontal--large color--blue font-size--medium text-align--center">
+        Start storing your personal recipes online to make sure they never get
+        lost. Keep them private, share them with the public or your friends
+        only.
+      </p>
+      <nuxt-link
+        to="/sign-up"
+        class="button button--large padding-horizontal--xlarge margin-top--large"
+      >Get started ➔</nuxt-link>
+    </div>
+    <div class="flex-center-container">
+      <kokeboka class="illustration illustration--kokeboka" />
+    </div>
+  </section>
+</template>
+
+<script>
+import kokeboka from "~/assets/graphics/veggies.svg";
+
+export default {
+  name: "initial-info-section",
+  components: {
+    kokeboka
+  }
+};
+</script>
--- frontend/pages/index.vue
+++ frontend/pages/index.vue
@@ -1,27 +1,6 @@
 <template>
   <div v-if="!user">
-    <section class="tablet-width margin--auto padding-horizontal--large margin-top--xxlarge">
-      <div
-        class="flex-center-container flex-center-container--column mobile-width padding--none margin--auto"
-      >
-        <h1
-          class="heading--display-font padding-horizontal--large"
-        >It's time to digitalize your cook book!</h1>
-        <p class="padding-horizontal--large color--blue font-size--medium text-align--center">
-          Start storing your personal recipes online to make sure they never get
-          lost. Keep them private, share them with the public or your friends
-          only.
-        </p>
-        <nuxt-link
-          to="/sign-up"
-          class="button button--large padding-horizontal--xlarge margin-top--large"
-          v-if="!user"
-        >Get started ➔</nuxt-link>
-      </div>
-      <div class="flex-center-container">
-        <kokeboka class="illustration illustration--kokeboka" />
-      </div>
-    </section>
+    <initial-info-section />
 
     <sign-up-section
       class="container--blue"
@@ -53,7 +32,7 @@
 import { GoogleProvider, auth } from "~/plugins/firebase.js";
 import googleLogo from "~/static/btn_google_light_normal_ios.svg";
 import { user } from "~/mixins/getCurrentUser.js";
-import kokeboka from "~/assets/graphics/veggies.svg";
+import InitialInfoSection from "~/components/InitialInfoSection/InititalInfoSection.vue";
 import SignUpSection from "~/components/SignUp/SignUpSection.vue";
 
 import RecipesFilter from "~/components/RecipesFilter/RecipesFilter.vue";
@@ -63,7 +42,7 @@ export default {
   name: "Home",
   layout: "fullwidth",
   components: {
-    kokeboka,
+    InitialInfoSection,
     SignUpSection,
     RecipesFilter,
     RecipesList
