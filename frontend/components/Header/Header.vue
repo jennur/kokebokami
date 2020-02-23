a9bb5e958beebdf6ee9203b73c95cabfc57981fa
Jenny Bonsak <jenny.bonsak@gmail.com>
2020-02-23 11:38:43 +0100

    Fix header functionality

--- frontend/components/Header/Header.vue
+++ frontend/components/Header/Header.vue
@@ -1,6 +1,6 @@
 <template>
-  <div>
-    <navigation />
+  <div :class="`header ${scrollDown ? '' : 'header--open'}`">
+    <navigation @toggleMenu="handleMenu" />
   </div>
 </template>
 
@@ -10,6 +10,33 @@ export default {
   name: "header-component",
   components: {
     Navigation
+  },
+  data() {
+    return { previousScrollValue: 0, scrollDown: false, menuOpen: false };
+  },
+  methods: {
+    handleMenu(status) {
+      this.menuOpen = status;
+      console.log("Status::", status);
+    },
+    handleScroll() {
+      let currentScrollValue = window.pageYOffset;
+      if (
+        currentScrollValue > 0 &&
+        currentScrollValue > this.previousScrollValue
+      ) {
+        this.previousScrollValue = currentScrollValue;
+        if (currentScrollValue >= 50 && !this.menuOpen) {
+          this.scrollDown = true;
+        }
+      } else {
+        this.previousScrollValue = currentScrollValue;
+        this.scrollDown = false;
+      }
+    }
+  },
+  created() {
+    window.addEventListener("scroll", this.handleScroll);
   }
 };
 </script>
