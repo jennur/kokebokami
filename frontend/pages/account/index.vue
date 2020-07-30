189b512c5e46541b24d68bba19f6e4ebaffc725a
Jenny Bonsak <jenny.bonsak@gmail.com>
2020-07-30 13:57:02 +0200

    Add logout button to dashboard

--- frontend/pages/account/index.vue
+++ frontend/pages/account/index.vue
@@ -1,12 +1,18 @@
 <template>
   <div>
     <breadcrumbs :routes="breadcrumbs" />
+
     <div class="flex-row margin-vertical--xxlarge">
       <gateway-link
         v-for="(gateway, index) in gateways"
         :key="`gateway-link-${index}`"
         :gateway="gateway"
       />
+    </div>
+    <div
+      class="full-width flex-column flex-column--align-right margin-vertical--large"
+    >
+      <button class="logout-button" @click="logOut">Log out</button>
     </div>
   </div>
 </template>
@@ -63,6 +69,12 @@ export default {
           }
         }
       ];
+    }
+  },
+  methods: {
+    logOut() {
+      this.$store.dispatch("USER_SIGN_OUT");
+      this.$router.push("/");
     }
   }
 };
