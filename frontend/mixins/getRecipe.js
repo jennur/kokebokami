fa1ae3b9a76755d48bde80236f8431fbde7cab4f
Jenny Bonsak <jenny.bonsak@gmail.com>
2020-09-06 20:45:19 +0200

    Fix metadata issue and access

--- frontend/mixins/getRecipe.js
+++ frontend/mixins/getRecipe.js
@@ -1,18 +1,112 @@
+import axios from "axios";
 export default {
   data() {
     return {
       userAuth: !!this.$fireAuth.currentUser,
-      recipe: [],
+      recipeKey: "",
+      recipe: {},
       recipeOwner: {},
       recipeLoaded: false,
       errorMessage: "",
-      structuredData: {}
+      structuredData: {},
+      headData: {}
     };
   },
-  computed: {
-    recipeKey() {
-      return this.$route.params.recipeid;
-    }
+  async asyncData({ params }) {
+    let recipeID = params.recipeid;
+    return await axios
+      .get(
+        `https://${process.env.PROJECT_ID}.firebaseio.com/recipes/${recipeID}.json?auth=${process.env.DATABASE_SECRET}`
+      )
+      .then(async recipe => {
+        if (recipe.data) {
+          recipe = recipe.data;
+          let recipeOwner = await axios
+            .get(
+              `https://${process.env.PROJECT_ID}.firebaseio.com/users/${recipe.ownerID}.json?auth=${process.env.DATABASE_SECRET}`
+            )
+            .then(user => {
+              if (user.data)
+                return {
+                  displayName: user.data.displayName,
+                  photoURL: user.data.photoURL,
+                  id: recipe.ownerID
+                };
+              return {};
+            })
+            .catch(error => console.log("Error getting recipe owner:", error));
+          let instructions = recipe.instructions;
+          instructions = instructions.map(instruction => {
+            return {
+              "@type": "HowToStep",
+              text: instruction
+            };
+          });
+          let categories = Array.isArray(recipe.categories)
+            ? recipe.categories.join(",")
+            : "";
+          let typeOfMeal = Array.isArray(recipe.typeOfMeal)
+            ? recipe.typeOfMeal.join(",")
+            : "";
+          let structuredData = {
+            "@context": "https://schema.org/",
+            "@type": "Recipe",
+            name: recipe.title,
+            image: [
+              recipe.photoURL ||
+                "https://firebasestorage.googleapis.com/v0/b/kokebokami-e788f.appspot.com/o/images%2Fglobals%2FrecipeImage%2Frecipe-backup-img.png?alt=media&token=8c3e8a1a-4226-453b-a256-0ac1060adb59"
+            ],
+            description: recipe.description || "",
+            keywords: categories,
+            recipeYield: recipe.servings || 0,
+            recipeCategory: typeOfMeal,
+
+            recipeIngredient: recipe.ingredients || [],
+            recipeInstructions: instructions || []
+          };
+          return {
+            recipe: recipe,
+            recipeKey: recipeID,
+            recipeOwner,
+            structuredData,
+            headData: {
+              title: `${recipe.title} | Kokebokami`,
+              meta: [
+                {
+                  property: "og:url",
+                  content: `https://wwww.kokebokami.com/recipes/${recipeID}/`
+                },
+                {
+                  property: "og:type",
+                  content: `article`
+                },
+                {
+                  property: "og:title",
+                  content: recipe.title || "Untitled recipe"
+                },
+                {
+                  property: "og:description",
+                  content: recipe.description || "No description available"
+                },
+                {
+                  property: "og:image",
+                  content:
+                    recipe.photoURL ||
+                    "https://firebasestorage.googleapis.com/v0/b/kokebokami-e788f.appspot.com/o/images%2Fglobals%2FrecipeImage%2Frecipe-backup-img.png?alt=media&token=8c3e8a1a-4226-453b-a256-0ac1060adb59"
+                }
+              ],
+              script: [
+                {
+                  type: "application/ld+json",
+                  json: structuredData
+                }
+              ]
+            }
+          };
+        }
+        return { recipe: { ownerID: null } };
+      })
+      .catch(error => console.log("Error getting recipe:", error.message));
   },
   methods: {
     getRecipe() {
@@ -20,59 +114,36 @@ export default {
       recipeRef
         .once("value", recipe => {
           if (recipe.exists()) {
-            if (this.userAuth || recipe.val().public === true) {
-              recipe = recipe.val();
-              let recipeOwnerRef = this.$fireDb.ref(`users/${recipe.ownerID}`);
-              recipeOwnerRef.once("value", owner => {
-                if (owner.exists()) this.recipeOwner = owner.val();
-              });
-              this.recipe = recipe;
-              this.recipeLoaded = true;
-              let instructions = recipe.instructions;
-              instructions = instructions.map(instruction => {
-                return {
-                  "@type": "HowToStep",
-                  text: instruction
-                };
-              });
-              let categories = Array.isArray(recipe.categories)
-                ? recipe.categories.join(",")
-                : "";
-              let typeOfMeal = Array.isArray(recipe.typeOfMeal)
-                ? recipe.typeOfMeal.join(",")
-                : "";
-              this.structuredData = {
-                "@context": "https://schema.org/",
-                "@type": "Recipe",
-                name: recipe.title,
-                image: [
-                  recipe.photoURL
-                    ? recipe.photoURL
-                    : "https://firebasestorage.googleapis.com/v0/b/kokebokami-e788f.appspot.com/o/images%2Fglobals%2FrecipeImage%2Frecipe-backup-img.png?alt=media&token=8c3e8a1a-4226-453b-a256-0ac1060adb59"
-                ],
-                description: recipe.description ? recipe.description : "",
-                keywords: categories,
-                recipeYield: recipe.servings ? recipe.servings : 0,
-                recipeCategory: typeOfMeal,
-
-                recipeIngredient: recipe.ingredients ? recipe.ingredients : [],
-                recipeInstructions: instructions ? instructions : []
-              };
-              return true;
-            }
-            this.$router.push("/404");
-            return false;
+            this.recipe = recipe.val();
           } else {
-            this.$router.push("/404");
-            return false;
+            this.recipe = { ownerID: null };
           }
         })
         .catch(error => {
           console.log("Error: Failed getting recipe:", error.message);
         });
+    },
+    checkIfSharedWithMe() {
+      let recipe = this.recipe;
+      let user = this.user;
+      if (recipe && user) {
+        if (recipe.ownerID === user.id) return true;
+        else if (user && recipe && recipe.sharedWith) {
+          return Object.values(recipe.sharedWith).indexOf(this.user.id) > -1;
+        }
+      }
+      return false;
     }
   },
   created() {
-    this.getRecipe();
+    if (
+      !this.recipe.ownerID ||
+      (this.recipe && !this.recipe.public && !this.checkIfSharedWithMe()) ||
+      (!this.userAuth && this.recipe && !this.recipe.public)
+    ) {
+      this.$router.push("/no-access");
+    } else {
+      this.recipeLoaded = true;
+    }
   }
 };
---
+++ frontend/pages/no-access.vue
@@ -0,0 +1,27 @@
+<template>
+  <main
+    class="mobile-width padding-horizontal--large margin--auto margin-bottom--xxlarge"
+  >
+    <div class="container container--center">
+      <h1 class="margin-top--xxlarge">Oh no!</h1>
+      <cracked-eggs class="illustration--cracked-eggs" />
+      <h3>
+        It looks like you don't have access to this recipe.
+      </h3>
+    </div>
+    <div class="container container--center">
+      <nuxt-link to="/">Discover public recipes</nuxt-link>
+    </div>
+  </main>
+</template>
+
+<script>
+import crackedEggs from "~/assets/graphics/illustrations/cracked-eggs.svg";
+export default {
+  name: "no-access",
+  components: {
+    crackedEggs
+  },
+  layout: "default"
+};
+</script>
