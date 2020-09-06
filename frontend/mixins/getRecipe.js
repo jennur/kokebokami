9f7551426d7d36439fe6fbee8c05032d8e7a3dbf
Jenny Bonsak <jenny.bonsak@gmail.com>
2020-09-06 21:30:10 +0200

    Run asyncData on server only

--- frontend/mixins/getRecipe.js
+++ frontend/mixins/getRecipe.js
@@ -12,82 +12,84 @@ export default {
     };
   },
   async asyncData({ params }) {
-    let recipeID = params.recipeid;
-    return await axios
-      .get(
-        `https://${process.env.PROJECT_ID}.firebaseio.com/recipes/${recipeID}.json?auth=${process.env.DATABASE_SECRET}`
-      )
-      .then(async recipe => {
-        if (recipe.data) {
-          recipe = recipe.data;
-          let instructions = recipe.instructions;
-          instructions = instructions.map(instruction => {
-            return {
-              "@type": "HowToStep",
-              text: instruction
-            };
-          });
-          let categories = Array.isArray(recipe.categories)
-            ? recipe.categories.join(",")
-            : "";
-          let typeOfMeal = Array.isArray(recipe.typeOfMeal)
-            ? recipe.typeOfMeal.join(",")
-            : "";
-          let structuredData = {
-            "@context": "https://schema.org/",
-            "@type": "Recipe",
-            name: recipe.title,
-            image: [
-              recipe.photoURL ||
-                "https://firebasestorage.googleapis.com/v0/b/kokebokami-e788f.appspot.com/o/images%2Fglobals%2FrecipeImage%2Frecipe-backup-img.png?alt=media&token=8c3e8a1a-4226-453b-a256-0ac1060adb59"
-            ],
-            description: recipe.description || "",
-            keywords: categories,
-            recipeYield: recipe.servings || 0,
-            recipeCategory: typeOfMeal,
-
-            recipeIngredient: recipe.ingredients || [],
-            recipeInstructions: instructions || []
-          };
-          return {
-            headData: {
-              title: `${recipe.title} | Kokebokami`,
-              meta: [
-                {
-                  property: "og:url",
-                  content: `https://wwww.kokebokami.com/recipes/${recipeID}/`
-                },
-                {
-                  property: "og:type",
-                  content: `article`
-                },
-                {
-                  property: "og:title",
-                  content: recipe.title || "Untitled recipe"
-                },
-                {
-                  property: "og:description",
-                  content: recipe.description || "No description available"
-                },
-                {
-                  property: "og:image",
-                  content:
-                    recipe.photoURL ||
-                    "https://firebasestorage.googleapis.com/v0/b/kokebokami-e788f.appspot.com/o/images%2Fglobals%2FrecipeImage%2Frecipe-backup-img.png?alt=media&token=8c3e8a1a-4226-453b-a256-0ac1060adb59"
-                }
+    if (process.server) {
+      let recipeID = params.recipeid;
+      return await axios
+        .get(
+          `https://${process.env.PROJECT_ID}.firebaseio.com/recipes/${recipeID}.json?auth=${process.env.DATABASE_SECRET}`
+        )
+        .then(async recipe => {
+          if (recipe.data) {
+            recipe = recipe.data;
+            let instructions = recipe.instructions;
+            instructions = instructions.map(instruction => {
+              return {
+                "@type": "HowToStep",
+                text: instruction
+              };
+            });
+            let categories = Array.isArray(recipe.categories)
+              ? recipe.categories.join(",")
+              : "";
+            let typeOfMeal = Array.isArray(recipe.typeOfMeal)
+              ? recipe.typeOfMeal.join(",")
+              : "";
+            let structuredData = {
+              "@context": "https://schema.org/",
+              "@type": "Recipe",
+              name: recipe.title,
+              image: [
+                recipe.photoURL ||
+                  "https://firebasestorage.googleapis.com/v0/b/kokebokami-e788f.appspot.com/o/images%2Fglobals%2FrecipeImage%2Frecipe-backup-img.png?alt=media&token=8c3e8a1a-4226-453b-a256-0ac1060adb59"
               ],
-              script: [
-                {
-                  type: "application/ld+json",
-                  json: structuredData
-                }
-              ]
-            }
-          };
-        }
-        return { recipe: { ownerID: null } };
-      })
-      .catch(error => console.log("Error getting recipe:", error.message));
+              description: recipe.description || "",
+              keywords: categories,
+              recipeYield: recipe.servings || 0,
+              recipeCategory: typeOfMeal,
+
+              recipeIngredient: recipe.ingredients || [],
+              recipeInstructions: instructions || []
+            };
+            return {
+              headData: {
+                title: `${recipe.title} | Kokebokami`,
+                meta: [
+                  {
+                    property: "og:url",
+                    content: `https://wwww.kokebokami.com/recipes/${recipeID}/`
+                  },
+                  {
+                    property: "og:type",
+                    content: `article`
+                  },
+                  {
+                    property: "og:title",
+                    content: recipe.title || "Untitled recipe"
+                  },
+                  {
+                    property: "og:description",
+                    content: recipe.description || "No description available"
+                  },
+                  {
+                    property: "og:image",
+                    content:
+                      recipe.photoURL ||
+                      "https://firebasestorage.googleapis.com/v0/b/kokebokami-e788f.appspot.com/o/images%2Fglobals%2FrecipeImage%2Frecipe-backup-img.png?alt=media&token=8c3e8a1a-4226-453b-a256-0ac1060adb59"
+                  }
+                ],
+                script: [
+                  {
+                    type: "application/ld+json",
+                    json: structuredData
+                  }
+                ]
+              }
+            };
+          }
+          return { recipe: { ownerID: null } };
+        })
+        .catch(error => console.log("Error getting recipe:", error.message));
+    }
   },
   computed: {
     recipeKey() {
