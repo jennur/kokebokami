45b877790b24e09991f472036bbe99fa7c8cadfb
Jenny Bonsak <jenny.bonsak@gmail.com>
2020-09-09 11:42:23 +0200

    Add meta for signup

--- frontend/pages/sign-up.vue
+++ frontend/pages/sign-up.vue
@@ -18,6 +18,25 @@ export default {
   head() {
     return {
       title: `Sign up | Kokebokami`,
+      meta: [
+        {
+          hid: "description",
+          name: "description",
+          content:
+            "Sign up to start building your personal cookbook with Kokebokami."
+        },
+        {
+          hid: "og:title",
+          property: "og:title",
+          content: "Sign up"
+        },
+        {
+          hid: "og:description",
+          property: "og:description",
+          content:
+            "Sign up to start building your personal cookbook with Kokebokami."
+        }
+      ],
       link: [
         {
           rel: "canonical",
