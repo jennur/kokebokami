4fa199006c65d85ec809d752fba7c4b589caa208
Jenny Bonsak <jenny.bonsak@gmail.com>
2021-01-15 16:37:43 +0100

    Fix url bug

--- frontend/helpers/generatePath.js
+++ frontend/helpers/generatePath.js
@@ -1,6 +1,12 @@
 export default function generatePath(title, key){
   let keySegment = key && key.replace(/[A-Z]/g, "").replace(/-/g, "").slice(0,5);
-  let path = title && title.replace(/ /g, "-").replace(/&/g, "and").toLowerCase();
+  let path = "";
 
+  if(title) {
+    path = title.replace(/ /g, "-")
+                .replace(/&/g, "and")
+                .replace(/\//g, "-")
+                .toLowerCase();
+  }
   return `${keySegment}/${path}/`;
 }
