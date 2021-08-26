5205c8d052c0582933675e4eba2e2acd677f183a
Jenny Bonsak <jenny.bonsak@gmail.com>
2021-08-26 20:43:33 +0200

    Fix date for comments

--- helpers/recipe-model.js
+++ helpers/recipe-model.js
@@ -1,4 +1,5 @@
 import generatePath from "./generatePath";
+import getDateString from "./get-date-string";
 
 export default function recipeModel(recipe, key){
   let title = recipe.title || "untitled-recipe";
@@ -22,27 +23,3 @@ function isSameDay(ISODate1, ISODate2){
   let date2 = new Date(ISODate2);
   return date1.toLocaleDateString() === date2.toLocaleDateString();
 }
-
-function getDateString(ISODateString){
-  let milliseconds = Date.parse(ISODateString);
-  let date = new Date(milliseconds);
-  date = new Date(date);
-  let time = date.toLocaleTimeString('en-GB', { timeZone: 'UTC', timeZoneName: 'short', hour: '2-digit', minute: '2-digit' })
-  return `${date.toLocaleDateString()} ${time}`;
-}
-
-// function createCommentObj(comment) {
-//   comment = {
-//     ...comment[1],
-//     key: comment[0]
-//   };
-//   let submitDate = comment.submitDate.replace(/"/g, "");
-//   let dateString = getDateString(submitDate);
-//   comment.submitDate = dateString;
-
-//   if (comment.isAnonymous) {
-//     comment.username = "Anonymous";
-//     comment.photoURL = "";
-//   }
-//   return comment;
-// }
