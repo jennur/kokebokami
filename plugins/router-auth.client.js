286925ef4ca94b91ed7789d7cf1e57b8de8789b9
jennur <jenny.bonsak@gmail.com>
2025-01-08 16:50:52 +0100

    router: fix redirect issues and cleanups

--- plugins/router-auth.client.js
+++ plugins/router-auth.client.js
@@ -1,9 +1,9 @@
-// Make sure router-auth runs client side on page load
-
+/* 
+ * This is added to make sure router-auth runs client side on page load,
+ * since the middleware only runs server side the first time.
+ */
 import routerAuth from "~/middleware/router-auth.js";
 
 export default function(context) {
-  context.app.router.afterEach(() => {
-    routerAuth(context);
-  });
+  context.app.router.afterEach(() => routerAuth(context));
 }
